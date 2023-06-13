1c8ddec5c03c8224a6da082799c335e81b721d52
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-06-13 08:49:25 +0400

    view models

--- ToDoList/ViewModels/ToDoListItemViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ToDoListItemViewModel.swift
-//  ToDoList
-//
-//  Created by Hasan Safarli on 6/13/23.
-//
-
-import Foundation
---
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoListItemViewModel.swift
+//  ToDoList
+//
+//  Created by Hasan Safarli on 6/13/23.
+//
+
+import Foundation
+
+//// ViewModel for single to do list item view (each row in items list)
+/// Primary tab
+class ToDoListItemViewViewModel: ObservableObject {
+    init() {}
+}
