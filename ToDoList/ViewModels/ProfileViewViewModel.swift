3a6003cc362b0bca6f8e7ed0c6f8e5763cf7737c
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-06-17 23:45:38 +0400

    log out

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -14,20 +14,7 @@ class ProfileViewViewModel: ObservableObject {
     
     init() {}
     
-    func toggleIsDone(item: ToDoListItem) {
-        var itemCopy = item
-        itemCopy.setDone(!item.isDone)
-        
-        guard let uid = Auth.auth().currentUser?.uid else {
-            return
-        }
-        
-        let db = Firestore.firestore()
-        db.collection("users")
-            .document(uid)
-            .collection("todos")
-            .document(itemCopy.id)
-            .setData(itemCopy.asDictionary() )
+    func logOut() {
         
     }
 }
--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,6 +13,33 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
+                // Avatar
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.blue)
+                    .frame(width: 125, height: 125)
+                
+                // Info
+                VStack (alignment: .leading) {
+                    HStack {
+                        Text("Name: ")
+                        Text("Hasan Safarli")
+                    }
+                    HStack {
+                        Text("Email: ")
+                        Text("hsnsfrli@gmail.com")
+                    }
+                    HStack {
+                        Text("Member since: ")
+                        Text("today")
+                    }
+                }
+                
+                // Sign out
+                TLButton(title: "Log out", background: .red) {
+                    viewModel.logOut()
+                }
                 
             }
             .navigationTitle("Profile")
